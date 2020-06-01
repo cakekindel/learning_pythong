4b1c57cabea7ca4552a222691ae88f92f0498486
Raine <greenneonraine@gmail.com>
2020-06-01 16:34:17 -0700

    add stubs

---
+++ find_odd_number.py/find_odd_number.py
@@ -0,0 +1,23 @@
+#requirements - given an array of numbers, find a number that appears an odd number of times|
+
+input = [1,1,1,2,2] 
+
+def find_odd_number (numbers):                       #looks at all numbers, tells you know which one is defined odd
+
+def is_odd (number):                                 #number picks a single number out
+
+def how_many_times (needle,haystack):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
