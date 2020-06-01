da83c0e7d8a856f37757184e1398895bc4f5effc
Raine <greenneonraine@gmail.com>
2020-06-01 16:13:59 -0700

    upload raine's fizzbuzz

---
+++ fizzbuzz/raine/Basics.py
@@ -0,0 +1,14 @@
+from math import pow 
+
+lol="fuckingnumbersmatewow"
+print (lol) 
+
+mynumber=69 
+mynumbernsq=pow(mynumber,2) 
+
+print (mynumbernsq)
+print (mynumber)
+
+
+#Pow = is a function wants an input what number to put to a power.
+#chumbucket
---
+++ fizzbuzz/raine/fizzbuzz.py
@@ -0,0 +1,24 @@
+#Requirments
+#input of some number, for soem given number, if divisable by 3, then print "fizz"
+#if by 5, print "buzz"
+#if by both, print "fizzbuzz"
+
+#define a function named fizzbuzz who takes num 
+def fizz_buzz (num) :
+    print ("lol loser")
+    if num % 3 == 0 and num % 5 == 0 : #if its divisable by 3 and 5, print out fizzbuzz
+        print ("fizzbuzz")
+    elif num % 3 == 0 : #else if it is only divisable by 3, print out fizz
+        print ("fizz")
+    elif num % 5 == 0 : #else if it is only divisable by 5, print out buzz
+        print ("buzz")
+
+
+fizz_buzz (15)
+
+
+#
+#
+#
+
+
