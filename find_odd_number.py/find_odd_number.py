603b4351ad5a9dc33bc5357c38dcd48a33f00968
alan <alanwenger545@gmail.com>
2020-06-01 16:53:27 -0700

    defined if_odd

--- find_odd_number.py/find_odd_number.py
+++ find_odd_number.py/find_odd_number.py
@@ -4,7 +4,16 @@ input = [1,1,1,2,2]
 
 def find_odd_number (numbers):                       #looks at all numbers, tells you know which one is defined odd
 
+
+
+
+
+
 def is_odd (number):                                 #number picks a single number out
+    if n % 2 == 0:
+        return false
+    if n % 2 != 0:
+        return true
 
 def how_many_times (needle,haystack):
 
