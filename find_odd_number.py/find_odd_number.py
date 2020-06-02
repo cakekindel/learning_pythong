859508bf1ac28aaaac7626ebd1ad846311516bd8
Orion Kindel <cakekindel@gmail.com>
2020-06-01 17:16:43 -0700

    start working on the main fn

--- find_odd_number.py/find_odd_number.py
+++ find_odd_number.py/find_odd_number.py
@@ -2,31 +2,23 @@
 
 input = [1,1,1,2,2] 
 
-def find_odd_number (numbers):                       #looks at all numbers, tells you know which one is defined odd
+# looks at all numbers, tells you know which one is in the list odd # of times
+def find_number_of_odd_count (numbers):
+    for num in numbers:
 
+        num_count = get_count(num, numbers)
 
+        if is_odd(num_count):
+            return num
 
-
-
-
-def is_odd (number):                                 #number picks a single number out
-    if n % 2 == 0:
+# returns true/false whether or not a number is odd
+def is_odd (number):
+    if number % 2 == 0:
         return false
-    if n % 2 != 0:
+    if number % 2 != 0:
         return true
 
-def how_many_times (needle,haystack):
-
-
-
-
-
-
-
-
-
-
-
-
-
+# get the number of times a number is in a list
+def get_count (needle, haystack):
+    return
 
